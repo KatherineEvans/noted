00511f6b9613e83d3a334aeff5e243ca2c821f3d
Katherine <Evans.Katherine.E@gmail.com>
2023-11-03 23:24:07 -0500

    job applications model/migration/controller

---
+++ database/migrations/2023_11_04_035640_create_job_applications_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('job_applications', function (Blueprint $table) {
+            $table->id();
+            $table->string('company');
+            $table->string('role');
+            $table->tinyInteger('location_type');
+            $table->string('location')->nullable();
+            $table->timestamp('date_applied')->nullable();
+            $table->tinyInteger('status')->default(1);
+            $table->text('note')->nullable();
+            $table->string('found_on')->nullable();
+            $table->string('projected_salary')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('job_applications');
+    }
+};
